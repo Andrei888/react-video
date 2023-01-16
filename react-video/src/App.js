12610c32a397df70224272283160af61246466e5
Andrei Purcaru <purcaruandrei20@yahoo.com>
2023-01-16 20:49:21 +0200

    up

--- react-video/src/App.js
+++ react-video/src/App.js
@@ -4,20 +4,41 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 import Header from "./components/Header";
 import Intro from "./components/Intro";
 import Boxlist from "./components/Boxlist";
 
+const clientTheme = createTheme({
+  status: {
+    danger: "#e53e3e",
+  },
+  palette: {
+    primary: {
+      main: "#04691f",
+      darker: "#053e85",
+    },
+    secondary: {
+      main: "#04691f",
+      darker: "#053e85",
+    },
+    neutral: {
+      main: "#64748B",
+      contrastText: "#fff",
+    },
+  },
+});
+
 function App() {
   const videoList = [
     {
-      title: "Learn React in 20 Minutes!! (3 projects for beginners)",
+      title: "Vue.js Course for Beginners [2021 Tutorial]",
       description:
-        "Welcome to our new video about 3 React projects for beginners! In this video, we will be showing you how to create three simple but useful React projects that are perfect for those new to the React framework.",
-      url: "https://www.youtube.com/embed/rQyB5yA13gE",
-      category: "react",
+        "Learn Vue 3 by in this full course. Vue.js is an open-source model–view–view model front end JavaScript framework for building user interfaces and single-page applications.",
+      url: "https://www.youtube.com/embed/FXpIoQ_rT_c",
+      category: "vue",
     },
     {
       title: "Learn React in 20 Minutes!! (3 projects for beginners)",
@@ -27,19 +48,64 @@ function App() {
       category: "react",
     },
     {
+      title:
+        "Angular for Beginners Course [Full Front End Tutorial with TypeScript]",
+      description:
+        "Learn Angular in this complete course for beginners. First you will learn the basics of Typescript and then you will learn about important Angular concepts such as binding, dependency injection, forms, routing, and more. ",
+      url: "https://www.youtube.com/embed/3qBXWUpoPHo",
+      category: "angular",
+    },
+    {
+      title: "Vue.js Course for Beginners [2021 Tutorial]",
+      description:
+        "Learn Vue 3 by in this full course. Vue.js is an open-source model–view–view model front end JavaScript framework for building user interfaces and single-page applications.",
+      url: "https://www.youtube.com/embed/FXpIoQ_rT_c",
+      category: "vue",
+    },
+    {
       title: "Learn React in 20 Minutes!! (3 projects for beginners)",
       description:
         "Welcome to our new video about 3 React projects for beginners! In this video, we will be showing you how to create three simple but useful React projects that are perfect for those new to the React framework.",
       url: "https://www.youtube.com/embed/rQyB5yA13gE",
       category: "react",
+    },
+    {
+      title:
+        "Angular for Beginners Course [Full Front End Tutorial with TypeScript]",
+      description:
+        "Learn Angular in this complete course for beginners. First you will learn the basics of Typescript and then you will learn about important Angular concepts such as binding, dependency injection, forms, routing, and more. ",
+      url: "https://www.youtube.com/embed/3qBXWUpoPHo",
+      category: "angular",
+    },
+    {
+      title: "Learn React in 20 Minutes!! (3 projects for beginners)",
+      description:
+        "Welcome to our new video about 3 React projects for beginners! In this video, we will be showing you how to create three simple but useful React projects that are perfect for those new to the React framework.",
+      url: "https://www.youtube.com/embed/rQyB5yA13gE",
+      category: "react",
+    },
+    {
+      title:
+        "Angular for Beginners Course [Full Front End Tutorial with TypeScript]",
+      description:
+        "Learn Angular in this complete course for beginners. First you will learn the basics of Typescript and then you will learn about important Angular concepts such as binding, dependency injection, forms, routing, and more. ",
+      url: "https://www.youtube.com/embed/3qBXWUpoPHo",
+      category: "angular",
+    },
+    {
+      title: "Vue.js Course for Beginners [2021 Tutorial]",
+      description:
+        "Learn Vue 3 by in this full course. Vue.js is an open-source model–view–view model front end JavaScript framework for building user interfaces and single-page applications.",
+      url: "https://www.youtube.com/embed/FXpIoQ_rT_c",
+      category: "vue",
     },
   ];
 
   const category = [
-    { category: "All", name: "All Videos" },
-    { category: "react", name: "React" },
-    { category: "angular", name: "Angular" },
-    { category: "vue", name: "Vue" },
+    { value: "-1", name: "All Videos" },
+    { value: "react", name: "React" },
+    { value: "angular", name: "Angular" },
+    { value: "vue", name: "Vue" },
   ];
   const [useVideos, setVideos] = useState(videoList);
   const [useCategory, setCategories] = useState("empty");
@@ -54,22 +120,23 @@ function App() {
 
   return (
     <div className="App ">
-      <CssBaseline />
-      <Container maxWidth="lg">
-        <Intro />
-        <header
-          filterFunction={filterHandler}
-          videos={videoList}
-          filteredVideos={useVideos}
-          activateCat={categoryHandler}
-          className="App-header"
-        >
-          <Header categories={category} />
-        </header>
-        <main>
-          <Boxlist items={useVideos} title="All Items" />
-        </main>
-      </Container>
+      <ThemeProvider theme={clientTheme}>
+        <CssBaseline />
+        <Container maxWidth="lg">
+          <Intro />
+          <header className="App-header">
+            <Header
+              allCategories={category}
+              filterFunction={filterHandler}
+              filterCat={categoryHandler}
+              videos={videoList}
+            />
+          </header>
+          <main>
+            <Boxlist items={useVideos} title="All Items" />
+          </main>
+        </Container>
+      </ThemeProvider>
     </div>
   );
 }
--- react-video/src/components/Header.jsx
+++ react-video/src/components/Header.jsx
@@ -1,21 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 import TextField from "@mui/material/TextField";
 
+function Header({ filterFunction, videos, allCategories, filterCat }) {
+  const [tabValue, setTabValue] = useState(allCategories[0].value);
+  const [searchInput, setSearchInput] = useState("");
 
-function Header({ categories }) {
+  useEffect(() => {
+    filterByCategory("-1");
+  }, []);
+
+  const HandlerSearch = (e) => {
+    setTabValue("-1");
+    filterByCategory("-1");
+    e.preventDefault();
+
+    setSearchInput(e.target.value);
+    console.log(searchInput);
+    if (searchInput.length > 0) {
+      const filtered = videos.filter((video) => {
+        return video.title.toLowerCase().includes(searchInput.toLowerCase());
+      });
+      filterFunction(filtered);
+    } else {
+      filterFunction(videos);
+    }
+  };
+  const filterByCategory = (category) => {
+    if (category !== "-1") {
+      const filtered = videos.filter((video) => {
+        return video.category === category;
+      });
+
+      filterFunction(filtered);
+    } else {
+      filterFunction(videos);
+    }
+
+    filterCat(category);
+  };
+
+  const categoryChange = (e, value) => {
+    e.preventDefault();
+    setTabValue(value);
+    filterByCategory(value);
+  };
+
   return (
     <div>
       <Stack spacing={2} direction="row">
-        {categories.map((category) => {
-          return (
-            <Button variant="outlined" type={category.category}>
-              {category.name}
-            </Button>
-          );
-        })}
-        <TextField id="outlined-search" label="Search field" type="search" />
+        <Tabs
+          value={tabValue}
+          onChange={categoryChange}
+          textColor="secondary"
+          indicatorColor="secondary"
+          aria-label="Main Navigation"
+        >
+          {allCategories.map((category) => {
+            return <Tab label={category.name} value={category.value} />;
+          })}
+        </Tabs>
+        <TextField
+          id="outlined-search"
+          label="Search field"
+          type="search"
+          value={searchInput}
+          onChange={HandlerSearch}
+        />
       </Stack>
     </div>
   );
